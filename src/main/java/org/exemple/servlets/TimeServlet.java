35a4ca2bab8ec8e091a88bda9ddde575ec167829
IvanShalaiev <ivanshalaev1990@gmail.com>
2024-01-23 18:11:00 +0100

    Fixed bug with uncorrect time zone displaing

--- src/main/java/org/exemple/servlets/TimeServlet.java
+++ src/main/java/org/exemple/servlets/TimeServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
@@ -15,16 +16,16 @@ public class TimeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html; charset=utf-8");
-        resp.getWriter().write(buildResponse(req));
+        resp.getWriter().write(getResponse(req));
         resp.getWriter().close();
     }
-
-    private String buildResponse(HttpServletRequest req) {
+//ZoneId.of("UTC")
+    private String getResponse(HttpServletRequest req) {
         StringBuilder sb = new StringBuilder();
-        sb.append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        sb.append(LocalDateTime.now(ZoneId.of(getQuery(req))).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         sb.append(" ");
         sb.append(getQuery(req));
-        return sb.toString();
+        return  sb.toString();
     }
 
     private String getQuery(HttpServletRequest req) {
